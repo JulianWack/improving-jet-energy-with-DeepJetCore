54c797022488fa63185517b173f45161f5b26890
Julian Wack <julian.wack@cern.ch>
2021-09-02 15:02:04 +0000

    fix loss function for last dense layers with more than one unit

--- modules/Losses.py
+++ modules/Losses.py
@@ -7,15 +7,16 @@ global_loss_list = {}
 def my_loss(y_true, y_pred):
     '''Finds the square difference between the genjet pt and the jet pt obtained by summing the products of constituent pt and network output.
     The loss is then found by the mean of the square errors for all jets.'''
-    
-    pt_correction_factor = y_pred[:,:,0]
-    consti_pt = y_pred[:,:,1]
+    # note that network output is (batch size, # jets, # units in final dense layer+1)
+    # the +1 is due to the PF pt passed thoruh the network, which will always be the last element
+    pt_correction_factor = tf.reduce_mean(y_pred[:,:,0:-1], axis=2)
+    consti_pt = y_pred[:,:,-1]
     consti_pt_corrected = consti_pt*pt_correction_factor
     jet_pt = tf.reduce_sum(consti_pt_corrected, axis=1)
     
     loss = ((jet_pt-y_true)/y_true)**2
     
-    return tf.reduce_mean(loss)
+    return loss
 
 
 global_loss_list['my_loss'] = my_loss
